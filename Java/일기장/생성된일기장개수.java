72273b5bec857bd112d1333814c64cf74ea770c6
MG <94948198+MingyeongKim0708@users.noreply.github.com>
2023-10-05 18:15:58 +0900

    Update 생성된일기장개수.java
    f 위에서 메세지창이 뜨도록 변경

--- Java/일기장/생성된일기장개수.java
+++ Java/일기장/생성된일기장개수.java
@@ -6,7 +6,7 @@ import javax.swing.JOptionPane;
 
 public class 생성된일기장개수 {
 
-	public void count() {
+	public void count(JFrame f) { //JFrame f를 설정함
 		File dir = new File("."); //프로젝트 위치
 		String[] list = dir.list();
 		int count = 0;
@@ -17,7 +17,7 @@ public class 생성된일기장개수 {
 				count++;
 			}
 		}
-		JOptionPane.showMessageDialog(null, count + "개의 txt 파일이 있습니다.");
+		JOptionPane.showMessageDialog(f, count + "개의 txt 파일이 있습니다."); //f 위에서 메세지창이 뜨도록 변경
 		System.out.println(count + "개의 txt 파일이 있습니다.");
 	}
 
